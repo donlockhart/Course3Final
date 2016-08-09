fb9ba326f7007595c93594e257e26622549ef1c9
Don Lockhart <don.r.lockhart@gmail.com>
2016-08-09 07:53:34 -0400

    Package check.

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,7 @@
+# Ensure required packages are installed
+list.of.packages <- c("dplyr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 library(dplyr)
 
 # Combine the three datasets into one for test data
